95be0833f6e26265c6d6f221072b0aabc6fb1604
Kirk Long <kirklong@u.boisestate.edu>
2020-01-07 21:47:53 -0700

    fixed shell script responsible for transferring files from server with scp

--- getMatchFiles.sh
+++ getMatchFiles.sh
@@ -2,11 +2,13 @@
 #this script takes inlist and finds relevant files
 #on Daryl's computer for LC processing later
 
-#code block below takes file separated by , and stores each col as
+#code block below takes file separated by , (with a space after comma) and stores each col as
 #variables obsid and src respectively
-while IFS=$'\,' read -r obsid src
+while IFS=$'\ ,' read -r obsid src
 do
-scp klong@claptrap:/Users/djm/Public/cxc/smc/${obsid}/primary/${obsid}_src_${src}.times ./test
+  filePath="/Users/djm/Public/cxc/smc/${obsid}/primary/${obsid}_src_${src}.times"
+  #echo $filePath
+  scp klong@claptrap:$filePath ./test
 done < "obsid_src_test.txt"
 
 #cd /Users/djm/Public/cxc/smc
