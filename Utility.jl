531c9968430d328327b4aa6867a06e1c48a8bd00
Kirk <kirklong@u.boisestate.edu>
2020-06-19 15:53:17 -0600

    updates to CHANDRA routine with a new sample plot, critical fix to gcdist routine in Utility.jl

--- Utility.jl
+++ Utility.jl
@@ -9,13 +9,19 @@ function gcdist(lon1,lat1,lon2,lat2) #Kirk changed
     lat1 = lat1*d2r
     lat2 = lat2*d2r
 
-    latdif =  lat2-lat1
-    try
-        rdist = acos(sin(lon1)*sin(lon2) + cos(lon1)*cos(lon2)*cos(latdif))
-        return rdist/d2r
-    catch
-        return 0
+    longdif =  abs(lon2-lon1)
+    acosArg = sin(lat1)*sin(lat1) + cos(lat1)*cos(lat1)*cos(longdif)
+    if acosArg > 1
+        acosArg = 1
     end
+    rdist = acos(acosArg)
+    return rdist/d2r
+    # try
+    #     rdist = acos(sin(lat1)*sin(lat1) + cos(lat1)*cos(lat1)*cos(longdif))
+    #     return rdist/d2r #return in degrees
+    # catch
+    #     return 0
+    # end
      #return 0 if acos arg is 1.0000000000000002 (result of consistent overflow problem)
 
 end
